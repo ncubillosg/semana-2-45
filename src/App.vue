bad322ee9d9671b2719300e6b17474000ae33a8e
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-12-14 20:38:58 -0500

    README Reemplazado

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,17 @@
 <template>
   <div id="app">
+    <Services />
   </div>
 </template>
 
 <script>
+import Services from './components/Services';
 
 export default {
   name: 'App',
+  components: {
+    Services
+  }
 }
 </script>
 
--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,12 +1,17 @@
 <template>
-    <div>
-
+    <div class="col-md-6 col-lg-3">
+        <div class="col-12 member">
+            <img :src="member.image" alt="imagen-integrante">
+            <h3 class="text-center">{{member.nombre}}</h3>
+            <h4 class="text-center">{{member.rol}}</h4>
+            <p class="text-center">{{member.descripcion}}</p>
+        </div>
     </div>
 </template>
 
 <script>
     export default {
-        name: "TeamCard",
+        name: "teamcard",
         props: ['member']
     }
 </script>
