7b8dec55775e3fbf842061845b2574b1260f9473
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-12-14 20:45:50 -0500

    Se agregaron estilos a las imágenes

--- src/components/TeamCard.vue
+++ src/components/TeamCard.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="col-md-6 col-lg-3">
         <div class="col-12 member">
-            <img :src="member.image" alt="imagen-integrante">
+            <img :src="member.image" :alt="member.nombre">
             <h3 class="text-center">{{member.nombre}}</h3>
             <h4 class="text-center">{{member.rol}}</h4>
             <p class="text-center">{{member.descripcion}}</p>
@@ -17,5 +17,9 @@
 </script>
 
 <style scoped>
-
+    img {
+        display: block;
+        margin: 0 auto;
+        max-width: 150px;
+    }
 </style>
