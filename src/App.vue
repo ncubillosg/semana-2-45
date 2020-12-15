03690a9367492d06fb3897098ec3b8388ec98abb
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-12-15 16:17:35 -0500

    Mueve el header y footer a componentes separados

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,24 @@
 <template>
   <div id="app">
-    <Team />
+    <Header />
+    <main>
+      <Team />
+    </main>
+    <Footer />
   </div>
 </template>
 
 <script>
 import Team from './components/Team';
+import Header from './components/Header';
+import Footer from './components/Footer';
 
 export default {
   name: 'App',
   components: {
-    Team
+    Team,
+    Header,
+    Footer
   }
 }
 </script>
---
+++ src/components/Footer.vue
@@ -0,0 +1,41 @@
+<template>
+  <footer
+            style="text-align: center; background-color: #3E4551; position: absolute; bottom: 0; width: 100%; color: white;">
+        <div class="container text-center text-md-left">
+            <div class="row">
+                <div class="col-md-5 mx-auto">
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Café de Colombia</h5>
+                    <p>Proyecto creado con la intención de promover el consumo de café colombiano, dando a conocer
+                        diferentes ofertas nacionales, cultivadas en todos las regiones del país y ofreciendo una
+                        calidad y sabor únicos.</p>
+                    <a href="https://github.com/dsaavedr/semana-1-45.git" target="_blank">GitHub del Proyecto</a>
+                </div>
+
+                <hr class="clearfix w-100 d-md-none">
+                <div class="col-md-6 mx-auto">
+                    <h5 class="font-weight-bold text-uppercase mt-3 mb-4">Contacto:</h5>
+                    <ul class="list-unstyled">
+                        <li>• Camilo González | Desarrollador Backend <br> Correo: cgonzalez@colcafe.com.co</li>
+                        <li>• Nicolás Cubillos | Desarrollador Backend<br> Correo: ncubillos@colcafe.com.co</li>
+                        <li>• Daniel Saavedra | Desarrollador Backend<br> Correo: dsaavedra@colcafe.com.co</li>
+                        <li>• Sebastian Cely | Desarrollador Backend<br> Correo: scely@colcafe.com.co</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <div class="footer-copyright text-center py-3" style="background-color: #323741;">
+            &copy; 2020 Copyright
+        </div>
+    </footer>
+</template>
+
+<script>
+export default {
+    name: "Footer"
+}
+</script>
+
+<style>
+
+</style>
